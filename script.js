4560df7f35adee215da92a448bf4037d504d4aaa
Jon Mendizabal <jonmendi@gmail.com>
2023-12-17 00:03:24 +0100

    initial commit, maybe last

---
+++ script.js
@@ -0,0 +1,49 @@
+const sarrera = "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";
+const input = document.querySelector("input");
+const submitButton = document.querySelector(".submit-button");
+const imageSection = document.querySelector(".images");
+const loadingMessage = document.querySelector(".loading-message");
+
+async function getImages() {
+  const imageToRemove = imageSection.querySelector(".image-container");
+  imageToRemove.remove()
+  loadingMessage.innerText = "Generating image...";
+  loadingMessage.style.display = "block";
+
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${sarrera}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: input.value,
+      n: 1,
+      size: "1024x1024",
+    }),
+  };
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      options
+    );
+    const data = await response.json();
+
+
+    data?.data.forEach((imageObject) => {
+      const imageContainer = document.createElement("div");
+      imageContainer.classList.add("image-container");
+      const imageElement = document.createElement("img");
+      imageElement.setAttribute("src", imageObject.url);
+      imageContainer.append(imageElement);
+      imageSection.append(imageContainer);
+    });
+  } catch (error) {
+    console.error(error);
+    loadingMessage.innerText = "Error fetching images. Please try again.";
+  } finally {
+    loadingMessage.style.display = "none";
+  }
+}
+
+submitButton.addEventListener("click", getImages);
