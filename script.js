09c1db9c1453efcbfd84a1a5ba77cd95c1da5852
Jon Mendizabal <jonmendi@gmail.com>
2023-12-18 11:57:35 +0100

    comment out API Key import

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import API_KEY from "./config.js";
+// import API_KEY from "./config.js";
 
 const keyInputElement = document.querySelector(".key-input");
 const startButton = document.querySelector(".start-button");
